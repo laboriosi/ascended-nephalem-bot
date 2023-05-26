edfd7cf832fa913a4c6e32ad6cf0bf15d865442c
Marcos Aurélio Pereira Júnior <marcos.aurelio@paketa.com.br>
2023-05-26 12:53:55 -0300

    fix: remove unused import

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
 import client from "~client";
-import {
-  ready,
-  voiceStateUpdate,
-  interactionCreate,
-  messageCreate,
-  messageReactionAdd,
-  channelCreate,
-  guildMemberAdd,
-} from "~events";
+import { ready, voiceStateUpdate, interactionCreate, messageCreate, channelCreate, guildMemberAdd } from "~events";
 import "dotenv/config";
 
 client.on("ready", ready);
 client.on("voiceStateUpdate", voiceStateUpdate);
 client.on("interactionCreate", interactionCreate);
 client.on("messageCreate", messageCreate);
-client.on("messageReactionAdd", messageReactionAdd);
 client.on("channelCreate", channelCreate);
 client.on("guildMemberAdd", guildMemberAdd);
 
