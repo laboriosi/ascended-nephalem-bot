95de6362ae6d2d9cb481ee2050732010608d25d9
Marcos Aurélio Pereira Júnior <marcos.aurelio@paketa.com.br>
2023-05-26 12:40:05 -0300

    chore: remove comments

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,6 +2,5 @@ export { default as ready } from "./ready";
 export { default as voiceStateUpdate } from "./voiceStateUpdate";
 export { default as interactionCreate } from "./interactionCreate";
 export { default as messageCreate } from "./messageCreate";
-export { default as messageReactionAdd } from "./messageReactionAdd";
 export { default as channelCreate } from "./channelCreate";
 export { default as guildMemberAdd } from "./guildMemberAdd";
--- src/events/interactionCreate/index.ts
+++ src/events/interactionCreate/index.ts
@@ -4,13 +4,16 @@ import { ChannelType, Interaction } from "~types";
 export default async (interaction: Interaction) => {
   try {
     const {
-      FORM_CATEGORY_ID,
-      MEMBER_ROLE_ID,
-      LEVELING_ROLE_ID,
-      PVP_ROLE_ID,
-      BOUNTY_ROLE_ID,
-      DUNGEON_ROLE_ID,
-      NIGHTMARE_DUNGEON_ROLE_ID,
+      FORM_CATEGORY_ID: formCategoryId,
+      MEMBER_ROLE_ID: memberRoleId,
+      LEVELING_ROLE_ID: levelingRoleId,
+      PVP_ROLE_ID: pvpRoleId,
+      BOUNTY_ROLE_ID: bountyRoleId,
+      DUNGEON_ROLE_ID: dungeonRoleId,
+      NIGHTMARE_DUNGEON_ROLE_ID: nightmareDungeonRoleId,
+      TICKET_CATEGORY_ID: ticketCategoryId,
+      OFFICER_ROLE_ID: officerRoleId,
+      HELPER_ROLE_ID: helperRoleId,
     } = process.env;
     const member = interaction.member as GuildMember;
     const user = member.user;
@@ -20,52 +23,84 @@ export default async (interaction: Interaction) => {
       if (interaction.customId === "leveling") {
         interaction.deferUpdate();
         const member = await interaction.guild.members.fetch(interaction.user.id);
-        if (member.roles.cache.has(LEVELING_ROLE_ID)) {
-          member.roles.remove(LEVELING_ROLE_ID);
+        if (member.roles.cache.has(levelingRoleId)) {
+          member.roles.remove(levelingRoleId);
         } else {
-          member.roles.add(LEVELING_ROLE_ID);
+          member.roles.add(levelingRoleId);
         }
       }
       if (interaction.customId === "pvp") {
         interaction.deferUpdate();
         const member = await interaction.guild.members.fetch(interaction.user.id);
-        if (member.roles.cache.has(PVP_ROLE_ID)) {
-          member.roles.remove(PVP_ROLE_ID);
+        if (member.roles.cache.has(pvpRoleId)) {
+          member.roles.remove(pvpRoleId);
         } else {
-          member.roles.add(PVP_ROLE_ID);
+          member.roles.add(pvpRoleId);
         }
       }
       if (interaction.customId === "bounty") {
         interaction.deferUpdate();
         const member = await interaction.guild.members.fetch(interaction.user.id);
-        if (member.roles.cache.has(BOUNTY_ROLE_ID)) {
-          member.roles.remove(BOUNTY_ROLE_ID);
+        if (member.roles.cache.has(bountyRoleId)) {
+          member.roles.remove(bountyRoleId);
         } else {
-          member.roles.add(BOUNTY_ROLE_ID);
+          member.roles.add(bountyRoleId);
         }
       }
       if (interaction.customId === "dungeon") {
         interaction.deferUpdate();
         const member = await interaction.guild.members.fetch(interaction.user.id);
-        if (member.roles.cache.has(DUNGEON_ROLE_ID)) {
-          member.roles.remove(DUNGEON_ROLE_ID);
+        if (member.roles.cache.has(dungeonRoleId)) {
+          member.roles.remove(dungeonRoleId);
         } else {
-          member.roles.add(DUNGEON_ROLE_ID);
+          member.roles.add(dungeonRoleId);
         }
       }
       if (interaction.customId === "nightmare_dungeon") {
         interaction.deferUpdate();
         const member = await interaction.guild.members.fetch(interaction.user.id);
-        if (member.roles.cache.has(NIGHTMARE_DUNGEON_ROLE_ID)) {
-          member.roles.remove(NIGHTMARE_DUNGEON_ROLE_ID);
+        if (member.roles.cache.has(nightmareDungeonRoleId)) {
+          member.roles.remove(nightmareDungeonRoleId);
         } else {
-          member.roles.add(NIGHTMARE_DUNGEON_ROLE_ID);
+          member.roles.add(nightmareDungeonRoleId);
         }
       }
       if (interaction.customId == "refuseRules") {
         interaction.reply({
           ephemeral: true,
-          content: `Parece receber o cargo de <@&${MEMBER_ROLE_ID}> você deve ler e concordar com as regras`,
+          content: `Parece receber o cargo de <@&${memberRoleId}> você deve ler e concordar com as regras`,
+        });
+      }
+
+      if (interaction.customId == "denounce") {
+        const channelName = `🎫┃denúncia-do-${user.username.toLowerCase()}`;
+
+        const createdChannel = await guild.channels.create({
+          name: channelName,
+          type: ChannelType.GuildText,
+          parent: ticketCategoryId,
+          permissionOverwrites: [
+            { id: member.id, allow: [PermissionsBitField.Default] },
+            { id: officerRoleId, allow: [PermissionsBitField.Default] },
+            { id: helperRoleId, allow: [PermissionsBitField.Default] },
+            { id: guild.roles.everyone, deny: [PermissionsBitField.All] },
+          ],
+        });
+      }
+
+      if (interaction.customId == "suggestion") {
+        const channelName = `🎫┃sugestão-do-${user.username.toLowerCase()}`;
+
+        const createdChannel = await guild.channels.create({
+          name: channelName,
+          type: ChannelType.GuildText,
+          parent: ticketCategoryId,
+          permissionOverwrites: [
+            { id: member.id, allow: [PermissionsBitField.Default] },
+            { id: officerRoleId, allow: [PermissionsBitField.Default] },
+            { id: helperRoleId, allow: [PermissionsBitField.Default] },
+            { id: guild.roles.everyone, deny: [PermissionsBitField.All] },
+          ],
         });
       }
 
@@ -82,7 +117,7 @@ export default async (interaction: Interaction) => {
           const createdChannel = await guild.channels.create({
             name: channelName,
             type: ChannelType.GuildText,
-            parent: FORM_CATEGORY_ID,
+            parent: formCategoryId,
             permissionOverwrites: [
               { id: member.id, allow: [PermissionsBitField.Default] },
               { id: guild.roles.everyone, deny: [PermissionsBitField.All] },
@@ -99,87 +134,4 @@ export default async (interaction: Interaction) => {
   } catch (error) {
     console.log(error);
   }
-  // const { FORM_CATEGORY_ID, MEMBER_ROLE_ID, WAITING_FORM_ROLE_ID, MEMBERS_TEXT_CHANNEL_ID, VISITANT_ROLE_ID } =
-  //   process.env;
-  // if (interaction.isButton() && interaction.customId === "recruitmentFormButton") {
-  //   const member = interaction.member as GuildMember;
-  //   const user = member.user;
-  //   const guild = member.guild;
-  //   const createdChannel = await guild.channels.create({
-  //     name: `📝┃form-do-${user.username}`,
-  //     type: ChannelType.GuildText,
-  //     parent: FORM_CATEGORY_ID,
-  //     permissionOverwrites: [
-  //       { id: member.id, allow: [PermissionsBitField.Default] },
-  //       { id: guild.roles.everyone, deny: [PermissionsBitField.All] },
-  //     ],
-  //   });
-  //   interaction.reply({
-  //     ephemeral: true,
-  //     content: `O formulário foi iniciado no canal <#${createdChannel.id}>`,
-  //   });
-  // }
-  // if (interaction.isButton() && interaction.customId === "formApprove") {
-  //   interaction.deferUpdate();
-  //   const embeds = interaction.message.embeds;
-  //   const [embed] = embeds;
-  //   const discordTag = embed.fields[1];
-  //   const guild = (interaction.member as GuildMember).guild;
-  //   const members = await guild.members.fetch();
-  //   const member = members.find((member) => member.user.tag === discordTag.value);
-  //   member.roles.remove(WAITING_FORM_ROLE_ID);
-  //   member.roles.add(MEMBER_ROLE_ID);
-  //   const membersChannel = (await guild.channels.fetch(MEMBERS_TEXT_CHANNEL_ID)) as TextChannel;
-  //   await membersChannel.send({ embeds });
-  //   await interaction.message.delete();
-  // }
-  // if (interaction.isButton() && interaction.customId === "formRefuse") {
-  //   interaction.deferUpdate();
-  //   const [embed] = interaction.message.embeds;
-  //   const discordTag = embed.fields[1];
-  //   const guild = (interaction.member as GuildMember).guild;
-  //   const members = await guild.members.fetch();
-  //   const member = members.find((member) => member.user.tag === discordTag.value);
-  //   member.roles.remove(WAITING_FORM_ROLE_ID);
-  //   member.roles.add(VISITANT_ROLE_ID);
-  //   await interaction.message.delete();
-  // }
-  // if (interaction.isButton() && interaction.customId === "formBlock") {
-  //   interaction.deferUpdate();
-  //   const [embed] = interaction.message.embeds;
-  //   const discordTag = embed.fields[1];
-  //   const guild = (interaction.member as GuildMember).guild;
-  //   const members = await guild.members.fetch();
-  //   const member = members.find((member) => member.user.tag === discordTag.value);
-  //   await interaction.message.delete();
-  //   member.roles.remove(WAITING_FORM_ROLE_ID);
-  // }
-  // if (interaction.isButton() && interaction.customId === "choose") {
-  //   interaction.deferUpdate();
-  //   const [embed] = interaction.message.embeds;
-  //   const teamOneColor = "#535353";
-  //   const teamTwoColor = "#74a3f3";
-  //   const pickIndex = picksDatabase.data.findIndex((pick) => pick.leaders.includes(interaction.user.id));
-  //   const pick = picksDatabase.data[pickIndex];
-  //   const hours = moment.duration(moment().diff(moment(pick.createdAt))).asHours();
-  //   if (pick && hours < 1) {
-  //     const teamPosition = pick.leaders.findIndex((leaderDiscordId) => leaderDiscordId === interaction.user.id) + 1;
-  //     const clickedPlayerIndex = pick.players.findIndex((player) => player.name === embed.author.name);
-  //     picksDatabase.data[pickIndex].players[clickedPlayerIndex] = {
-  //       ...picksDatabase.data[pickIndex].players[clickedPlayerIndex],
-  //       team: String(teamPosition),
-  //     };
-  //     interaction.message.edit({
-  //       embeds: [
-  //         new EmbedBuilder()
-  //           .setColor(teamPosition === 1 ? teamOneColor : teamTwoColor)
-  //           .setAuthor({ name: embed.author.name, iconURL: embed.author.iconURL }),
-  //       ],
-  //       components: [],
-  //     });
-  //     fs.writeFile("src/database/picks.json", JSON.stringify(picksDatabase), (err) => {
-  //       console.log(err);
-  //     });
-  //   }
-  // }
 };
