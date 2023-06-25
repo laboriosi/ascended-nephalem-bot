52c4fa69f9f7ecbd70a196b42d08b136daf9d5d8
Marcos Aurélio Pereira Júnior <marcos.aurelio@paketa.com.br>
2023-06-25 09:12:21 -0300

    feat: update guild rules

--- src/embeds/rules.ts
+++ src/embeds/rules.ts
@@ -11,6 +11,7 @@ export default new EmbedBuilder()
 **6. Sem ameaças diretas e indiretas.** Ameaças a outros usuários de DDoS, morte, DoX, abuso e outras ameaças maliciosas são absolutamente proibidas e proibidas.
 **7. Discord obrigatório.** Se um membro estiver no jogo e não no Discord sem motivos claros para tal, o mesmo será removido do Clã.
 **8. Sem debates acalorados sobre política e religião.** Nossa intenção é ficarmos unidos, independente de qual ideologia seguimos; sendo assim é melhor evitarmos tais tópicos pela saúde do ambiente.
+**9. Ações que quebram o ToS do Diablo 4.** Fazer ou anúnciar que irá fazer algo que quebra os termos de serviço do Diablo 4 é algo punível com banimento da guilda.
 `
   )
   .setColor("#da373c");
